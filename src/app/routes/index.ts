9146643fd539c927ee73790ceaedbc018cccae8f
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-11-10 16:23:23 +0600

    Starting work on user module

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,15 +1,24 @@
-import express from 'express';
-
+import express from "express";
+import { userRoutes } from "../modules/user/user.route";
 
 const router = express.Router();
 
-const moduleRoutes = [
-    {
-        path: '/',
-        route: router
-    }
+interface Route {
+  path: string;
+  route: express.Router;
+}
+
+const moduleRoutes: Route[] = [
+  {
+    path: "/",
+    route: router,
+  },
+  {
+    path: "/users",
+    route: userRoutes,
+  },
 ];
 
-moduleRoutes.forEach(route => router.use(route.path, route.route))
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
 export default router;
