a49996b56279cfa1b18b443b80f7ea7e63c1c4e1
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-11-14 23:40:25 +0600

    fetch doc data

--- src/app/modules/doctorSchedule/doctorSchedule.service.ts
+++ src/app/modules/doctorSchedule/doctorSchedule.service.ts
@@ -1,4 +1,11 @@
+import prisma from "../../shared/prisma";
+
 const insertIntoDB = async (user: any, payload: any) => {
+  const doctorData = await prisma.doctor.findUniqueOrThrow({
+    where: {
+      email: user.email,
+    },
+  });
   return { user, payload };
 };
 
