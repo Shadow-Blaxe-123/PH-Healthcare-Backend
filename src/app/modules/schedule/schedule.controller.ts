caa557a6f0e55e5f8837f447f3a9bbd2d75ccde8
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-11-14 22:29:31 +0600

    filter and pagination options extraction for get route

--- src/app/modules/schedule/schedule.controller.ts
+++ src/app/modules/schedule/schedule.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import catchAsync from "../../shared/catchAsync";
 import { ScheduleService } from "./schedule.service";
 import sendResponse from "../../shared/sendResponse";
+import pick from "../../helper/pick";
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   const result = await ScheduleService.insertIntoDB(req.body);
@@ -14,7 +15,10 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
 });
 
 const schedulesForDoctor = catchAsync(async (req: Request, res: Response) => {
-  const result = await ScheduleService.schedulesForDoctor();
+  const options = pick(req.query, ["page", "limit", "sortBy", "sort"]);
+  const filters = pick(req.query, ["startDateTime", "endDateTime"]);
+
+  const result = await ScheduleService.schedulesForDoctor(filters, options);
   sendResponse(res, {
     statusCode: 201,
     success: true,
