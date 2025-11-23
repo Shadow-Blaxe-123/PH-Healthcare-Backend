8e6ca0d0e7ff79a8c1277428f27810a5166d9c53
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-11-23 23:21:33 +0600

    create appointment part 2

--- src/app/modules/appointment/appointment.service.ts
+++ src/app/modules/appointment/appointment.service.ts
@@ -1,9 +1,28 @@
 import { IJWTPayload } from "../../interfaces";
+import prisma from "../../shared/prisma";
 
 const createAppointment = async (
   user: IJWTPayload,
   payload: { doctorId: string; scheduleId: string }
-) => {};
+) => {
+  const patientData = await prisma.patient.findUniqueOrThrow({
+    where: {
+      email: user.email,
+    },
+  });
+  const doctorData = await prisma.doctor.findUniqueOrThrow({
+    where: {
+      id: payload.doctorId,
+    },
+  });
+  const isBookedOrNot = await prisma.doctorSchedules.findFirstOrThrow({
+    where: {
+      scheduleId: payload.scheduleId,
+      doctorId: payload.doctorId,
+      isBooked: false,
+    },
+  });
+};
 
 export const AppointmentService = {
   createAppointment,
